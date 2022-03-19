9b3e5ffbfe631f63298ca276a09ac6c9670418c2
negu63 <negu6633@gmail.com>
2022-03-19 22:28:46 +0900

    Migration to Typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,10 +4,15 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
+interface LayoutProps {
+  children: JSX.Element;
+  home?: boolean;
+};
+
 const name = "negu63";
 export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: LayoutProps) {
   return (
     <div className={styles.container}>
       <Head>
